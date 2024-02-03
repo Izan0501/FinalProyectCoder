b9769ce46dee36a72c83e30941a4bc4133a7c8be
Izan <ivozanacchi501@gmail.com>
2024-02-03 16:28:06 -0300

    addFinalFunctionsAndStyles

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
 /**scrollReveal */
-const scrollR = ScrollReveal ({
+const scrollR = ScrollReveal({
     distance: '65px',
     duration: 2600,
     delay: 450,
     reset: true
 });
-scrollR.reveal('.Cardscarousel', {delay:250, origin:'left'});
-scrollR.reveal('.container', {delay:250, origin:'top'});
-scrollR.reveal('.contentTxt', {delay:100, origin:'left'});
-scrollR.reveal('.slider2', {delay:400, origin:'top'});
-scrollR.reveal('.icons', {delay:500, origin:'left'});
-scrollR.reveal('.sectionName', {delay:500, origin:'left'});
-scrollR.reveal('.cardsSection', {delay:750, origin:'left'});
-scrollR.reveal('.d-flex', {delay:650, origin:'top'});
-scrollR.reveal('.footer', {delay:700, origin:'left'});
+
+scrollR.reveal('.Cardscarousel', { delay: 250, origin: 'left' });
+scrollR.reveal('.container', { delay: 250, origin: 'top' });
+scrollR.reveal('.contentTxt', { delay: 100, origin: 'left' });
+scrollR.reveal('.slider2', { delay: 400, origin: 'top' });
+scrollR.reveal('.icons', { delay: 500, origin: 'left' });
+scrollR.reveal('.sectionName', { delay: 500, origin: 'left' });
+scrollR.reveal('.cardsSection', { delay: 750, origin: 'left' });
+scrollR.reveal('.d-flex', { delay: 650, origin: 'top' });
+scrollR.reveal('.footer', { delay: 700, origin: 'left' });
 
 /**menuList-funcions */
 
@@ -27,87 +28,103 @@ menu.addEventListener('click', (e) => {
 
 /**carousel1-functions */
 
-document.querySelector("#next").onclick = function() {
+document.querySelector("#next").onclick = function () {
     let lists = document.querySelectorAll(".item");
     document.querySelector("#slide").appendChild(lists[0]);
 }
 
-document.querySelector("#prev").onclick = function() {
+document.querySelector("#prev").onclick = function () {
     let lists = document.querySelectorAll(".item");
     document.querySelector("#slide").prepend(lists[lists.length - 1]);
 }
 
 /**carousel2-functions */
 
-function App() {}
-    window.onload = function(event) {
-        let app = new App();
-        window.app = app;
+function App() { }
+window.onload = function (event) {
+    let app = new App();
+    window.app = app;
+}
+App.prototype.processingButton = function (event) {
+    const btn = event.currentTarget;
+    const carruselList = event.currentTarget.parentNode;
+    const track = event.currentTarget.parentNode.querySelector("#track");
+    const carrusel = track.querySelectorAll(".carrusel");
+
+    const carruselWidth = carrusel[0].offsetWidth;
+    const trackWidth = track.offsetWidth;
+    const listWidth = carruselList.offsetWidth;
+
+    track.style.left == "" ? leftPosition = track.style.left = 0 : leftPosition = parseFloat(track.style.left.slice(0, -2) * -1)
+    btn.dataset.button == "button-prev" ? prevAction(leftPosition, carruselWidth, track) : nextAction(leftPosition, trackWidth, listWidth, carruselWidth, track)
+
+
+}
+
+/*Next and Prev sliderProducts functions */ 
+let prevAction = (leftPosition, carruselWidth, track) => {
+    if (leftPosition > 0) {
+        track.style.left = `${-1 * (leftPosition - carruselWidth)}px`;
+
     }
-    App.prototype.processingButton = function (event) {
-        const btn = event.currentTarget;
-        const carruselList = event.currentTarget.parentNode;
-        const track = event.currentTarget.parentNode.querySelector("#track");
-        const carrusel = track.querySelectorAll(".carrusel");
+}
 
-        const carruselWidth = carrusel[0].offsetWidth;
-        const trackWidth = track.offsetWidth;
-        const listWidth = carruselList.offsetWidth;
-
-        track.style.left == "" ? leftPosition = track.style.left = 0 : leftPosition = parseFloat(track.style.left.slice(0,-2) * -1)
-        btn.dataset.button == "button-prev" ? prevAction(leftPosition, carruselWidth, track) : nextAction(leftPosition,trackWidth, listWidth, carruselWidth, track)
-
-
+let nextAction = (leftPosition, trackWidth, listWidth, carruselWidth, track) => {
+    if (leftPosition < (trackWidth - listWidth)) {
+        track.style.left = `${-1 * (leftPosition + carruselWidth)}px`;
     }
+}
 
-    let prevAction = (leftPosition, carruselWidth, track) => {
-        if(leftPosition > 0) {
-            track.style.left = `${-1 * (leftPosition - carruselWidth)}px`;
-
-        }
-    }
-
-    let nextAction = (leftPosition, trackWidth, listWidth, carruselWidth, track) => {
-        if (leftPosition < (trackWidth - listWidth)) {
-            track.style.left = `${-1 * (leftPosition + carruselWidth)}px`;
-        }
-    }
-
- /**cartProducts-functions */
-
+/**cartProduct-functions */
 const cart = document.querySelector('#cart');
 const buttons = document.querySelectorAll('.btn-outline');
 const template = document.querySelector('#template');
 const footer = document.querySelector('#dFooter');
 const templateFooter = document.querySelector('#templateFooter');
-const countString = document.querySelector ('#pill')
+const countString = document.querySelector('#pill');
 const fragment = document.createDocumentFragment();
 
 let countProduct = 0
 let cartProducts = [];
 
+/**add Product to cart function */
 const addCartProduct = (e) => {
     const product = {
         tittle: e.target.dataset.game,
         id: e.target.dataset.game,
         amount: 1,
-        count:0,
-        price: parseInt( e.target.dataset.price),
+        count: 0,
+        price: parseInt(e.target.dataset.price),
         img: e.target.dataset.img,
     }
 
     const position = cartProducts.findIndex(item => {
         return item.tittle === product.tittle
     })
-    if(position === -1) {
-        cartProducts.push(product);   
+
+    if (position === -1) {
+        cartProducts.push(product);
     } else {
         cartProducts[position].amount++
     }
+
+
     showCart()
+
 };
 
+/**save products to localStorage */
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('cartProducts')) {
+        cartProducts = JSON.parse(localStorage.getItem('cartProducts'));
+        showCart();
+    }
+})
+
+/**show cartProducts function */
 const showCart = () => {
+    localStorage.setItem('cartProducts', JSON.stringify(cartProducts));
+
     cart.textContent = ''
 
     cartProducts.forEach(item => {
@@ -121,23 +138,36 @@ const showCart = () => {
 
         clone.querySelector('.btn-success').dataset.id = item.id
         clone.querySelector('.btn-danger').dataset.id = item.id
+        clone.querySelector('.delete-button').dataset.id = item.id
+        //countString.textContent = cartProducts.length;
 
-        countString.textContent = countProduct; 
 
         fragment.appendChild(clone)
+
     });
 
     cart.appendChild(fragment)
 
+    countProducts()
+
     showFooter()
 }
 
+/** product counter function*/
+const countProducts = () => {
+    countString.style.display = 'block'
+    countString.innerText = cartProducts.length
+}
+
+/**show templateFooter */
 const showFooter = () => {
     footer.textContent = '';
 
+
     const total = cartProducts.reduce((acc, current) => {
-        return acc + current.amount * current.price;
+        return acc + current.amount * current.price
     }, 0);
+
     console.log(total)
 
     const clone = templateFooter.content.cloneNode(true)
@@ -146,22 +176,24 @@ const showFooter = () => {
     footer.appendChild(clone)
 }
 
+/**function of increasing quantity */
 const btnAdd = (e) => {
     cartProducts = cartProducts.map((item) => {
         if (e.target.dataset.id === item.id) {
             item.amount++;
         } else {
             countProduct.textContent = '';
-            countProduct ++;
+            countProduct++;
         }
         return item;
     });
     showCart();
 };
 
+/**quantity subtraction function */
 const btnDelete = (e) => {
-    cartProducts = cartProducts.filter (item => {
-        if(e.target.dataset.id === item.id){
+    cartProducts = cartProducts.filter(item => {
+        if (e.target.dataset.id === item.id) {
             if (item.amount > 0) {
                 item.amount--;
                 if (item.amount === 0) return
@@ -169,28 +201,45 @@ const btnDelete = (e) => {
         } else {
             countProduct.textContent = '';
             countProduct--;
-
         };
+
         return item
+
     });
-    showCart()
+
+    showCart();
 };
 
+/**buttons events*/
 document.addEventListener('click', (e) => {
-    
-   if(e.target.matches('.btn-outline')) {
-    addCartProduct(e);
-    btnAdd(e)
-    e.preventDefault();
-   }
 
-   if (e.target.matches('.btn-success')) {
-    btnAdd(e)
-    e.preventDefault()
-   }
-   if (e.target.matches('.btn-danger')) {
-    btnDelete(e);
-    e.preventDefault()
-}
+
+    if (e.target.matches('.btn-outline')) {
+
+        Swal.fire({
+            icon: "success",
+            title: "Congratulations",
+            text: "we successfully save your purchase!",
+        });
+
+        addCartProduct(e);
+        btnAdd(e)
+        e.preventDefault();
+    }
+
+    if (e.target.matches('.btn-success')) {
+        btnAdd(e)
+        e.preventDefault()
+    }
+
+    if (e.target.matches('.btn-danger')) {
+        btnDelete(e);
+        e.preventDefault()
+    }
+
+    if (e.target.matches('.delete-button')) {
+        btnDelete(e)
+        e.preventDefault()
+    }
 });
 
